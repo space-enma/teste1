b672c941b3a366bc815d2fcef251e404ec5ba1f6
lucianononato <lucianononato33@gmail.com>
2024-04-15 19:37:03 -0300

    Sua mensagem de commit aqui

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('title', 'Space Enma')
-    
+
 @section('content')
 <section class="sobre">
   <h1 class="sobre-h1">Um puco sobre a Nossa empresa </h1>
@@ -13,7 +13,7 @@
               conteudos gratuitos em nossas redes sociais.
           </h3>
       </div>
-      <div class="sobre-area2"> 
+      <div class="sobre-area2">
           <h1>Nossa Historia</h1>
           <h3>
           &nbsp;&nbsp; olá fazemos parte da Space Enma, Luciano Nonato e leandro Nonato  e os proprietários da empresa, a empresa começou com o intuito de criar
@@ -21,11 +21,11 @@
           em um mundo onde todos estão conectados não ter um site pode trazer um prejuízo e também um site dá outra visão do negócio.
 
           Também vimos que para ter um site era um processo bem trabalhoso e custoso além da nossa empresa cuidar disso para você,
-          temos um dos melhores preços para criação de site sem ter dores de cabeças. 
+          temos um dos melhores preços para criação de site sem ter dores de cabeças.
           </h3>
       </div>
   </div>
-      
+
 </section>
 
 @endsection
